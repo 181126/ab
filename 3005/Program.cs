5aea1dea7afe255568aa5a3c16832a6d018bf220
Renato Suares <renatorsc2006@gmail.com>
2023-06-06 14:03:17 -0400

    Segundo commit

---
+++ 3005/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Poo
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe o primeiro valor: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Informe o segundo valor: ");
+        int b = int.Parse(Console.ReadLine());
+        int result = a / b;
+        Console.WriteLine(result);
+
+        Console.ReadKey();
+    }
+}
